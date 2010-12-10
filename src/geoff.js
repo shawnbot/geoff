b113d17ae0cfb0b51299925cf49adc4bee504f20
Shawn Allen <shawn@stamen.com>
2010-12-10 14:25:54 -0800

    initial commit

---
+++ src/geoff.js
@@ -0,0 +1,409 @@
+if (!geoff) var geoff = {};
+(function(geoff) {
+
+ 	/**
+	 * Convert a GeoJSON geometry coordinate [x,y] array to a WGS94 object with
+	 * "lat" and "lon" attributes.
+	 */
+ 	geoff.ctoll = function(coord) {
+		return {lat: coord[1], lon: coord[0]};
+	};
+
+ 	/**
+	 * Convert a WGS94 object with "lat" and "lon" attributes to a GeoJSON
+	 * geometry coordinate [x,y] array.
+	 */
+ 	geoff.lltoc = function(latlon) {
+		return [latlon.lon, latlon.lat];
+	};
+
+ 	/**
+	 * Create a GeoJSON Point feature from a single WGS84 lat/lon object and an
+	 * optional properties object.
+	 */
+	geoff.lltoPoint = function(latlon, properties) {
+		return geoff.feature.Point(geoff.lltoc(latlon), properties);
+	};
+
+	/**
+	 * GeoJSON feature generators.
+	 */
+	geoff.feature = (function() {
+		// generate an arbitrary GeoJSON feature.
+		var feature = function(type, coordinates, properties) {
+			if (type == "GeometryCollection") {
+				return {
+					type: "Feature",
+					properties: properties || {},
+					geometry: {
+						type: type,
+						geometries: coordinates
+					}
+				};
+			}
+			return {
+				type: "Feature",
+				properties: properties || {},
+				geometry: {
+					type: type,
+					coordinates: coordinates || []
+				}
+			};
+		};
+
+		feature.Point = function(point, properties) {
+			return feature("Point", geoff.lltoc(point), properties);
+		};
+
+		feature.Polygon = function(rings, properties) {
+			return feature("Polygon", rings, properties);
+		};
+
+		feature.LineString = function(points, properties) {
+			return feature("LineString", points, properties);
+		};
+
+		feature.MultiLineString = function(lines, properties) {
+			return feature("MultiLineString", lines, properties);
+		};
+
+		feature.MultiPolygon = function(polygons, properties) {
+			return feature("MultiPolygon", polygons, properties);
+		};
+
+		feature.MultiPoint = function(points, properties) {
+			return feature("MultiPoint", points, properties);
+		};
+
+		function clone(obj, deep) {
+			if (obj instanceof Array) {
+				return deep
+					? obj.map(function(o) { return clone(o, true); })
+					: obj.slice();
+			} else {
+				var out = {};
+				for (var p in obj) {
+					out[p] = deep ? clone(obj[p], true) : obj[p];
+				}
+			}
+		}
+
+		function merge(a, b) {
+			for (var p in a) {
+				b[p] = (typeof a[p] == "object")
+					? merge(a[p], b[p])
+					: a[p];
+			}
+		}
+
+		feature.clone = function(feature, properties) {
+			var copy = clone(feature);
+			if (properties) merge(properties, feature.properties);
+			return copy;
+		};
+
+		return feature;
+	})();
+
+	/**
+	 * An extent is a lat/lon bounding box. Extents an be used to calculate
+	 * feature bounding boxes, determine
+	 */
+	geoff.extent = function(coords) {
+		// By default an extent is infinite.
+		var extent = {
+			min: {lon: Infinity, lat: Infinity},
+			max: {lon: -Infinity, lat: -Infinity}
+		};
+
+		// cover the globe
+		extent.world = function() {
+			extent.min = {lon: -180, lat: -90};
+			extent.max = {lon: +180, lat: +90};
+			return extent;
+		};
+
+		// Northwest, Southeast, Northeast and Southwest corner getter/setters
+		extent.northwest = function(latlon) {
+			if (arguments.length) {
+				extent.min.lon = latlon.lon;
+				extent.max.lat = latlon.lat;
+				return extent;
+			} else {
+				return {lat: extent.max.lat, lon: extent.min.lon};
+			}
+		};
+		extent.southeast = function(latlon) {
+			if (arguments.length) {
+				extent.max.lon = latlon.lon;
+				extent.min.lat = latlon.lat;
+				return extent;
+			} else {
+				return {lat: extent.min.lat, lon: extent.max.lon};
+			}
+		};
+		extent.northeast = function(latlon) {
+			if (arguments.length) {
+				extent.max.lon = latlon.lon;
+				extent.max.lat = latlon.lat;
+				return extent;
+			} else {
+				return {lat: extent.max.lat, lon: extent.max.lon};
+			}
+		};
+		extent.southwest = function(latlon) {
+			if (arguments.length) {
+				extent.min.lon = latlon.lon;
+				extent.max.lat = latlon.lat;
+				return extent;
+			} else {
+				return {lat: extent.max.lat, lon: extent.min.lon};
+			}
+		};
+
+		// Reset the extent to infinite bounds so that min/max calculations work out.
+		extent.reset = function() {
+			extent.min = {lon: Infinity, lat: Infinity};
+			extent.max = {lon: -Infinity, lat: -Infinity};
+			return extent;
+		};
+
+		// Enclose a GeoJSON [x,y] coordinate
+		extent.encloseCoordinate = function(coord) {
+			extent.min.lon = Math.min(extent.min.lon, coord[0]);
+			extent.max.lon = Math.max(extent.max.lon, coord[0]);
+			extent.min.lat = Math.min(extent.min.lat, coord[1]);
+			extent.max.lat = Math.max(extent.max.lat, coord[1]);
+			return extent;
+		};
+
+		// Enclose a WGS84 lat/lon location
+		extent.encloseLocation = function(latlon) {
+			extent.encloseCoordinate([latlon.lon, latlon.lat]);
+			return extent;
+		};
+
+		// Returns true if the extent contains the given lat/lon location
+		extent.containsLocation = function(latlon) {
+			return !(
+				latlon.lon < extent.min.lon ||
+				latlon.lat < extent.min.lat ||
+				latlon.lon > extent.max.lon ||
+				latlon.lat > extent.max.lat
+			);
+		};
+
+		// Returns true if the extent contains the given GeoJSON [x,y] coordinate
+		extent.containsCoordinate = function(coord) {
+			return !(
+				coord[0] < extent.min.lon ||
+				coord[1] < extent.min.lat ||
+				coord[0] > extent.max.lon ||
+				coord[1] > extent.max.lat
+			);
+		};
+
+		// Returns true if the extent contains the provided extent
+		extent.containsExtent = function(other) {
+			return extent.containsLocation(other.min)
+				&& extent.containsLocation(other.max);
+		};
+
+		// Returns true if the extent intersects the other one
+		extent.intersects = function(other) {
+			return !(
+				extent.min.lon > other.max.lon ||
+				extent.min.lat > other.max.lat ||
+				extent.max.lon < other.min.lon ||
+				extent.max.lat < other.min.lat ||
+			);
+		};
+
+		// Get the intersection of two extents as a new extent object if they
+		// intersect one another, or null if they don't.
+		extent.intersection = function(other) {
+			if (extent.intersects(other)) {
+				var result = geoff.extent();
+				result.min.lon = Math.max(extent.min.lon, other.min.lon);
+				result.min.lat = Math.max(extent.max.lat, other.max.lat);
+				result.max.lon = Math.min(extent.max.lon, other.max.lon);
+				result.max.lat = Math.min(extent.min.lat, other.min.lat);
+			}
+			return null;
+		};
+
+		// Generate a Polygon feature from this extent
+		extent.toFeature = function(properties) {
+			var ring = extent.ring();
+			return geoff.feature.Polygon([ring], properties);
+		};
+
+		// Get the "area" of the rectangle.
+		extent.area = function() {
+			return Math.abs((extent.max.lon - extent.min.lon) * (extent.max.lat - extent.min.lat));
+		};
+
+		// Generate a GeoJSON coordinate ring corresponding to the four corners of
+		// the extent rectangle
+		extent.ring = function() {
+			return [
+				[extent.min.lon, extent.max.lat],
+				[extent.max.lon, extent.max.lat],
+				[extent.max.lon, extent.min.lat],
+				[extent.min.lon, extent.min.lat]
+			];
+		};
+
+		// Buffer the extent by x and y values. If no y is provided we assume the x
+		// value for both dimensions.
+		extent.buffer = function(x, y) {
+			if (arguments.length == 0) {
+				throw new TypeError("Expecting 1 or two arguments for extent.buffer() (got zero)");
+			}
+			if (arguments.length == 1) {
+				y = x;
+			}
+			if (x != 0 && !isNaN(x)) {
+				extent.min.lon -= x;
+				extent.max.lon += x;
+			}
+			if (y != 0 && !isNaN(y)) {
+				extent.min.lat -= y;
+				extent.max.lat += y;
+			}
+			return extent;
+		};
+
+		// Enclose a feature geometry.
+		extent.encloseGeometry = function(geom) {
+			switch (geom.type) {
+				// Point geometries contain a single [x,y] coordinate
+				case "Point":
+					// enclose all of the point coordinates
+					var center = geom.coordinates;
+					extent.encloseCoordinate(center);
+					break;
+				// LineString and MultiPoint geometries contain a list of coordinates:
+				// [[x,y], [x,y]]
+				case "LineString":
+				case "MultiPoint":
+					// enclose all of the point coordinates
+					geom.coordinates.forEach(extent.encloseCoordinate);
+					break;
+				// MultiLineString and Polygon geometries contain a list of rings:
+				// [[[x,y], [x,y]],
+        //  [[x,y], [x,y]]]
+				case "MultiLineString":
+				case "Polygon":
+					// enclose each of the points in each ring
+					geom.coordinates.forEach(function(ring) {
+						ring.forEach(extent.encloseCoordinate);
+					});
+					break;
+				// MultiPolygon geometries contain a list of polygonal ring lists:
+				// [[[[x,y], [x,y]], [[x,y], [x,y]]],
+        //  [[[x,y], [x,y]], [[x,y], [x,y]]]]
+				case "MultiPolygon":
+					// enclose each of the points of each ring in each polygon
+					geom.coordinates.forEach(function(polygon) {
+						polygon.forEach(function(ring) {
+							ring.forEach(extent.encloseCoordinate);
+						});
+					});
+					break;
+
+				// For GeometryCollection geometries, apply extent.encloseGeometry() to
+				// each item in the "geometries" list.
+				case "GeometryCollection":
+					geom.geometries.forEach(extent.encloseGeometry);
+					break;
+			}
+			return extent;
+		};
+
+		// Enclose a single feature.
+		extent.encloseFeature = function(feature) {
+			return extent.encloseGeometry(feature.geometry);
+		};
+
+		return extent;
+	};
+
+	/**
+	 * Modify a feature so that includes an external rectangular ring buffer of
+	 * the given size (in lat/lon degrees) around the original coordinates.
+	 */
+	geoff.buffer = function(feature, buffer, fidelity) {
+		var extent = geoff.extent(),
+				geom = feature.geometry;
+
+		// enclose the feature's geometry in the extent
+		extent.encloseFeature(feature);
+		// then buffer it by the provided value
+		extent.buffer(buffer);
+
+		// then, maniplate the geometry...
+		switch (geom.type) {
+			case "Point":
+				// Turn it into a polygon with two rings:
+				// 1) the extent rectangle, and
+				// 2) an inner circle around the point coordinate
+				var center = geom.coordinates;
+				geom.type = "Polygon";
+				geom.coordinates = [
+					extent.ring(),
+					geoff.circle(center, buffer / 2, fidelity)
+				];
+				break;
+
+			case "Line":
+			case "LineString":
+				throw new TypeError("Can't do Line or LineString yet!");
+
+			case "MultiPoint":
+				// turn it into a polygon with 2 or more rings:
+				// 1) the extent rectangle, and
+				// 2+) circles around each of point coordinates
+				var coords = geom.coordinates.slice();
+				geom.type = "Polygon";
+				geom.coordinates = [extent.ring()];
+				coords.forEach(function(coord) {
+					geom.coordinates.push(geoff.circle(coord, buffer / 2, fidelity));
+				});
+				break;
+
+			case "Polygon":
+				// buffer the extent and prepend the geometry rings with the
+				// extent rectangle
+				extent.buffer(buffer);
+				geom.coordinates.unshift(extent.ring());
+				break;
+
+			case "MultiPolygon":
+				// buffer the extent and prepend a polygon with a the extent
+				// rectangle as its only ring
+				extent.buffer(buffer);
+				geom.coordinates.unshift([extent.ring()]);
+				break;
+		}
+		return feature;
+	};
+
+	var TWO_PI = Math.PI * 2;
+	// Generate a circular ring around the given coordinate with a given radius
+	// (beware latitudinal distortion!) and number of line segments (fidelity).
+	geoff.circle = function(coord, radius, fidelity) {
+		if (!radius) return [coord];
+		// TODO: deal with latitudinal distortion
+		if (!fidelity) fidelity = 32;
+		var points = [],
+				step = TWO_PI / fidelity;
+		for (var a = 0; a < TWO_PI; a += step) {
+			var x = coord[0] + radius * Math.sin(a),
+					y = coord[1] + radius * Math.cos(a);
+			points.push([x, y]);
+		}
+		return points;
+	};
+
+})(geoff);
